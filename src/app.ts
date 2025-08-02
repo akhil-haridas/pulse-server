fee49b7ada96a4227a2092952b896c91baa0b401
akhil-haridas <4khilharidas@gmail.com>
2025-08-02 18:09:18 +0530

    added log route app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import { authRoutes, userRoutes } from "@routes";
+import { authRoutes, logRoutes, userRoutes } from "@routes";
 
 dotenv.config();
 
@@ -14,6 +14,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/logs", logRoutes);
 
 // Health check route
 app.get("/api/health", (_req, res) => {
