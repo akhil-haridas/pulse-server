b267a702f655146d3019f6140928bf95fb9e5d49
akhil-haridas <4khilharidas@gmail.com>
2025-08-02 15:20:44 +0530

    Enhance signToken function to accept StringValue for expiration time

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,5 +1,7 @@
 import jwt, { SignOptions } from "jsonwebtoken";
 
+import type { StringValue } from "ms";
+
 const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!JWT_SECRET) {
@@ -8,7 +10,7 @@ if (!JWT_SECRET) {
 
 export const signToken = (
     payload: string | object | Buffer,
-    expiresIn: number
+    expiresIn: number | StringValue
 ) => {
     const options: SignOptions = { expiresIn };
     return jwt.sign(payload, JWT_SECRET, options);
