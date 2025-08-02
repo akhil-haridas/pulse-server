875263eda08a89910c49757779697ff739341145
akhil-haridas <4khilharidas@gmail.com>
2025-08-02 15:22:17 +0530

    Implement authentication routes with validation for register and login

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,1 +1,12 @@
-// Auth-related routes
+import express from "express";
+
+import { validate } from "@middlewares";
+import { login, register } from "@controllers";
+import { loginSchema, registerSchema } from "@validations";
+
+const router = express.Router();
+
+router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
+
+export default router;
