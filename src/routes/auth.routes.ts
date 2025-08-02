753c61a25a7d22a7995524d626094499a5a0d01c
akhil-haridas <4khilharidas@gmail.com>
2025-08-02 17:40:25 +0530

    refresh token route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,14 @@
 import express from "express";
 
 import { validate } from "@middlewares";
-import { login, register } from "@controllers";
+import { login, refreshAccessToken, register } from "@controllers";
 import { loginSchema, registerSchema } from "@validations";
 
 const router = express.Router();
 
 router.post("/register", validate(registerSchema), register);
 router.post("/login", validate(loginSchema), login);
+
+router.post("/refresh-token", refreshAccessToken);
 
 export default router;
