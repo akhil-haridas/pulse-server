0142c16d39a596f4f686111924015609fd789651
akhil-haridas <4khilharidas@gmail.com>
2025-08-02 17:53:16 +0530

    add user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { authenticate, AuthenticatedRequest } from "@middlewares";
+import { authenticate, validate } from "@middlewares";
+import { getMe, updateMe } from "@controllers";
+import { updateProfileSchema } from "@validations";
 
 const router = express.Router();
 
-router.get("/me", authenticate, (req: AuthenticatedRequest, res) => {
-    res.json({ message: "You are authenticated!", user: req.user });
-});
+router.get("/me", authenticate, getMe);
+router.put("/me", authenticate, validate(updateProfileSchema), updateMe);
 
 export default router;
